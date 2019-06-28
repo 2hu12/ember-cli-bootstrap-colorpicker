a6ee31d9a2c29329b5bac3d36d503cad45cac2eb
2hu <zhulongzheng@gmail.com>
2019-06-28 13:02:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ module.exports = {
 
   included() {
     let current = this;
+    let app;
 
     // Keep iterating upward until we don't have a grandparent.
     // Has to do this grandparent check because at some point we hit the project.
